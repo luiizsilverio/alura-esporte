481228d0cbe1e9ae5680fb8337f2139cf78f4a00
luiizsilverio <luiiz.silverio@gmail.com>
2023-03-31 20:29:47 -0300

    módulo 01 ok

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth/react-native";
+import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
@@ -19,4 +20,7 @@ const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(AsyncStorage)
 })
 
-export { auth }
+const db = getFirestore(app);
+
+
+export { auth, db }
--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
 
 import estilos from './estilos';
 import Cabecalho from '../../componentes/Cabecalho';
 import Produto from '../../componentes/Produtos';
-import { auth } from '../../config/firebase';
+import { auth, db } from '../../config/firebase';
 
 export default function Principal({ navigation }) {
   const usuario = auth.currentUser;
@@ -13,6 +14,21 @@ export default function Principal({ navigation }) {
     auth.signOut();
     navigation.replace('Login');
   }
+
+  useEffect(() => {
+    async function criarProduto() {
+      // await setDoc(doc(db, "produtos", "1234567"), {
+      //   nome: "tênis Mizuno",
+      //   preco: 120
+      // })
+      const docRef = await addDoc(collection(db, 'produtos'), {
+        nome: "tênis Mizuno",
+        preco: 120
+      })
+    }
+
+    // criarProduto();
+  }, [])
 
   return (
     <View style={estilos.container}>
